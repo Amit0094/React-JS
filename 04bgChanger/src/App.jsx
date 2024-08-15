e984cc93893c990baa772a731cefecdf85ed7574
Amit <amitroy9546184327@gmail.com>
2024-08-15 20:56:32 +0530

    bg-changer app

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <div className="w-full h-screen duration-200" style={{backgroundColor : color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-2xl bg-white rounded-xl px-3 py-2">
+            <button onClick={()=> setColor('red')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'red'}}>Red</button>
+
+            <button onClick={()=> setColor('green')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'green'}}>Green</button>
+
+            <button onClick={()=> setColor('pink')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'pink'}}>Pink</button>
+
+            <button  onClick={()=> setColor('blue')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'blue'}}>Blue</button>
+
+            <button onClick={()=> setColor('black')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'black'}}>Black</button>
+            <button onClick={()=> setColor('salmon')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'salmon'}}>Black</button>
+            <button onClick={()=> setColor('orange')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'orange'}}>Black</button>
+            <button onClick={()=> setColor('violet')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'violet'}}>Black</button>
+            <button onClick={()=> setColor('yellow')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'yellow'}}>Black</button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
